853709f9683a45e9903958ada09e59af6aa220a5
Cristina Cristobal <cristina.cristobal@gmail.com>
2018-12-29 18:22:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,7 +37,7 @@ shipping_manifest["muskets"] => 2
   # add 4 gun powder to the shipping_manifest hash below
 shipping_manifest["gun powder"] => 4
 
-
+end
   # return the shipping_manifest hash below
 shipping_manifest
 end
